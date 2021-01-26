f89dfe88bfb5abbffcb482751e5c63a0ddd541d9
Cagdas Enc <cagdas22edirne@gmail.com>
2021-01-26 14:20:06 -0500

    initial commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Hello {
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+
+    }
+
+
 }
--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class HelloTest {
+public class HelloTest {
 }
